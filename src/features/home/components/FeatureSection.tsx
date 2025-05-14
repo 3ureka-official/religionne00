11ee30d8e19505a86532662920083ce994035781
sota <sota.matsui@akane.waseda.jp>
2025-05-14 13:01:06 +0900

    fix: ページネーションの矢印

--- src/features/home/components/FeatureSection.tsx
+++ src/features/home/components/FeatureSection.tsx
@@ -8,9 +8,9 @@ const ProductCard = ({ featured = false }) => {
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
   return (
-    <Paper 
-      elevation={0} 
-      sx={{ 
+    <Paper
+      elevation={0}
+      sx={{
         overflow: 'hidden',
         border: featured ? '1px solid rgba(128,128,128,0.35)' : 'none',
         height: '100%',
@@ -19,8 +19,8 @@ const ProductCard = ({ featured = false }) => {
       }}
     >
       {featured && (
-        <Typography 
-          sx={{ 
+        <Typography
+          sx={{
             p: 1,
             fontSize: { xs: '20px', sm: '24px' },
             fontWeight: featured ? 'normal' : 'medium',
@@ -29,16 +29,16 @@ const ProductCard = ({ featured = false }) => {
           pick up!!!
         </Typography>
       )}
-      <Box 
-        sx={{ 
-          bgcolor: '#D9D9D9', 
-          pt: '100%', 
+      <Box
+        sx={{
+          bgcolor: '#D9D9D9',
+          pt: '100%',
           position: 'relative',
           flex: featured ? '1 0 auto' : 'none',
         }}
       >
-        <Box 
-          sx={{ 
+        <Box
+          sx={{
             position: 'absolute',
             top: '50%',
             left: '50%',
@@ -46,14 +46,14 @@ const ProductCard = ({ featured = false }) => {
           }}
         >
           <svg xmlns="http://www.w3.org/2000/svg" width={isMobile ? "25" : "35"} height={isMobile ? "25" : "35"} fill="#757575" viewBox="0 0 16 16">
-            <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"/>
-            <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+            <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" />
+            <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
           </svg>
         </Box>
       </Box>
       <Box sx={{ p: isMobile ? 0.5 : 1 }}>
-        <Typography 
-          sx={{ 
+        <Typography
+          sx={{
             fontSize: { xs: '11px', sm: featured ? '15px' : '14px' },
             fontWeight: 'normal',
             mb: 0.5
@@ -61,8 +61,8 @@ const ProductCard = ({ featured = false }) => {
         >
           商品名
         </Typography>
-        <Typography 
-          sx={{ 
+        <Typography
+          sx={{
             fontSize: { xs: '11px', sm: featured ? '15px' : '14px' },
             fontWeight: 'normal'
           }}
@@ -79,10 +79,10 @@ const CategorySelector = () => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
   const categories = ['Original', 'Tops', 'Bottoms', 'Jackets', 'Coat', 'Others']
-  
+
   return (
-    <Box 
-      sx={{ 
+    <Box
+      sx={{
         display: 'flex',
         justifyContent: 'space-between',
         mb: { xs: 2, sm: 4 },
@@ -91,9 +91,9 @@ const CategorySelector = () => {
       }}
     >
       {categories.map((category, index) => (
-        <Typography 
+        <Typography
           key={index}
-          sx={{ 
+          sx={{
             fontSize: { xs: '12px', sm: '14px' },
             fontWeight: index === 0 ? 'bold' : 'normal',
             color: 'black',
@@ -112,10 +112,10 @@ const Pagination = () => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
   const pages = [1, 2, 3, 4, 5]
-  
+
   return (
-    <Box 
-      sx={{ 
+    <Box
+      sx={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -123,23 +123,23 @@ const Pagination = () => {
         mt: { xs: 3, sm: 4 }
       }}
     >
-      <Box 
-        sx={{ 
+      <Box
+        sx={{
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          transform: 'rotate(90deg)'
+          transform: 'rotate(-90deg)'
         }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width={isMobile ? "16" : "20"} height={isMobile ? "16" : "20"} fill="#555555" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+          <path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
         </svg>
       </Box>
-      
+
       {pages.map((page, index) => (
-        <Box 
+        <Box
           key={index}
-          sx={{ 
+          sx={{
             width: isMobile ? 24 : 30,
             height: isMobile ? 24 : 30,
             display: 'flex',
@@ -155,17 +155,17 @@ const Pagination = () => {
           {page}
         </Box>
       ))}
-      
-      <Box 
-        sx={{ 
+
+      <Box
+        sx={{
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          transform: 'rotate(-90deg)'
+          transform: 'rotate(90deg)'
         }}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width={isMobile ? "16" : "20"} height={isMobile ? "16" : "20"} fill="#555555" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+          <path fillRule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
         </svg>
       </Box>
     </Box>
@@ -180,12 +180,12 @@ const FeatureSection = () => {
     <Box>
       {/* カテゴリーセレクター */}
       <CategorySelector />
-      
+
       {/* ピックアップ商品 */}
       <Box sx={{ mb: { xs: 3, sm: 6 } }}>
         <ProductCard featured={true} />
       </Box>
-      
+
       {/* 商品グリッド */}
       <Box sx={{ display: 'flex', flexWrap: 'wrap', mx: isMobile ? -0.5 : -1 }}>
         {Array(6).fill(null).map((_, index) => (
@@ -194,7 +194,7 @@ const FeatureSection = () => {
           </Box>
         ))}
       </Box>
-      
+
       {/* ページネーション */}
       <Pagination />
     </Box>
